c50f79dff69be8a846fc7300f615aebf9287716c
Saikat Sarkar <ssarkar925@gmail.com>
2025-09-01 06:03:07 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'sarkar-co',
-  appId: '1:476613728632:web:e8acb2aea46b409db6108d',
-  storageBucket: 'sarkar-co.firebasestorage.app',
-  apiKey: 'AIzaSyCg12itK1XBGtAwVd2ZxM35LcxGJpy_p4c',
-  authDomain: 'sarkar-co.firebaseapp.com',
-  messagingSenderId: '476613728632',
+  "projectId": "sarkar-co",
+  "appId": "1:476613728632:web:e8acb2aea46b409db6108d",
+  "storageBucket": "sarkar-co.firebasestorage.app",
+  "apiKey": "AIzaSyCg12itK1XBGtAwVd2ZxM35LcxGJpy_p4c",
+  "authDomain": "sarkar-co.firebaseapp.com",
+  "messagingSenderId": "476613728632",
 };
 
 // Initialize Firebase
