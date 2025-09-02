70da4a0e3d854f125775131adca1a8ea36b2b3ca
Saikat Sarkar <ssarkar925@gmail.com>
2025-09-02 08:10:33 +0000

    didn't select any dropdown vendor and customer for purchase bill and inv

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -70,9 +70,9 @@ export function Combobox({
               {options.map((option) => (
                 <CommandItem
                   key={option.value}
-                  value={option.label} // Use label for filtering
-                  onSelect={() => {
-                    onSelect(option.value === value ? "" : option.value)
+                  value={option.value}
+                  onSelect={(currentValue) => {
+                    onSelect(currentValue === value ? "" : currentValue)
                     setOpen(false)
                   }}
                 >
