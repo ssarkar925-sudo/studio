c5daa9db3189b4109115549b8154b64aa9957909
Saikat Sarkar <ssarkar925@gmail.com>
2025-09-06 15:16:32 +0000

    Using flatDir should be avoided because it doesn't support any meta-data

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'SC Billing',
-  webDir: 'public',
+  webDir: 'out',
+  server: {
+    url: 'http://192.168.0.102:9002',
+    cleartext: true,
+  },
 };
 
 export default config;
